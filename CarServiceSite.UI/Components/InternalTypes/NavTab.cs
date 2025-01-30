a03c3d1569b6e779df2c6aa482c6006a0174dea4
Петр Брякин <petr.bryakin@gmail.com>
2025-01-30 19:48:55 +0300

    finish TabsLayout

---
+++ CarServiceSite.UI/Components/InternalTypes/NavTab.cs
@@ -0,0 +1,8 @@
+﻿namespace CarServiceSite.UI.Components.InternalTypes
+{
+    public class NavTab
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+    }
+}
