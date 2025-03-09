ad26106b3f835c9f2fe3bfa87658a0de872a388b
Петр Брякин <petr.bryakin@gmail.com>
2025-03-09 17:46:35 +0300

    add default settings for auth

---
+++ CarServiceSite.Core/InputModels/UserInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServiceSite.Core.InputModels
+{
+    public class UserInputModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string Phone {  get; set; }
+        public string? BirthDate { get; set; }
+        public string? Email { get; set; }
+        public string Password { get; set; }
+    }
+}
