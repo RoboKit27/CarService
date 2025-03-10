468bc5e571f2d21c36ab04d72d24a8ab14c7e7f9
Петр Брякин <petr.bryakin@gmail.com>
2025-03-10 20:20:21 +0300

    ыефкы

---
+++ CarServiceSite.UI/Components/InternalTypes/RatingStar.cs
@@ -0,0 +1,17 @@
+﻿namespace CarServiceSite.UI.Components.InternalTypes
+{
+    public class RatingStar
+    {
+        public int Id { get; set; } = -1;
+        public bool IsActive { get; set; } = false;
+
+        public string GetCurrentImage()
+        {
+            switch (this.IsActive)
+            {
+                case true: return "images/active-star.png";
+                case false: return "images/disable-star.png";
+            }
+        }
+    }
+}
